3d149272247c23ba1e27ec8100c9e104f98bf9d8
Trilicia Gomes <triliciamgomes@gmail.com>
2026-08-15 00:58:51 -0300

    Sprint 2
    Tratamento de dados, padronização e análise de inconsistências

--- Mini_projeto.py
+++ Mini_projeto.py
@@ -20,3 +20,37 @@ print(df.dtypes)
 
 print("\nInformações gerais da base:")
 df.info()
+
+#Limpeza, conversão e padronização de tipos de dados
+df["DATA"] = pd.to_datetime(
+    df["DATA"],
+    format="%d/%m/%Y",
+    errors="coerce"
+)
+print("\nTipo da coluna DATA após conversão:")
+print(df["DATA"].dtype)
+print("\nQuantidade de datas inválidas:")
+print(df["DATA"].isna().sum())
+
+print("\nValores da coluna gênero:")
+print(df["CL_GENERO"].value_counts(dropna=False))
+
+print("\nValores da coluna segmento:")
+print(df["CL_SEG"].value_counts(dropna=False))
+
+print("\nValores da coluna categoria:")
+print(df["PR_CAT"].value_counts(dropna=False))
+
+print("\nQuantidade de produtos diferentes:")
+print(df["PR_NOME"].nunique())
+
+print("\nExemplos de produtos:")
+print(df["PR_NOME"].unique()[:20])
+
+df["PR_NOME"] = df["PR_NOME"].str.strip().str.upper()
+df["PR_NOME"] = df["PR_NOME"].str.replace(r"\s+", " ", regex=True)
+
+print("\nValores da coluna número de filhos:")
+print(df["CL_FHL"].value_counts(dropna=False).sort_index())
+print("\nMenor número de filhos:", df["CL_FHL"].min())
+print("Maior número de filhos:", df["CL_FHL"].max())
